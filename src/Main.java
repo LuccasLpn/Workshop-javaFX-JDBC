0feaa18270ce39d3f2d5ea4f1558c5ca324cfa80
Luccas Pereira <Luccas.lpn@outlook.com>
2021-12-19 16:35:16 -0300

    Main View Controller

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@
 import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
@@ -13,18 +12,15 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         
         try {
-            
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-            ScrollPane scrollpane = loader.load();
-            
+            ScrollPane scrollpane = loader.load();     
             scrollpane.setFitToHeight(true);
             scrollpane.setFitToWidth(true);
-            
-            
             Scene mainScene = new Scene(scrollpane);
             primaryStage.setScene(mainScene);
             primaryStage.setTitle("Sample JavaFX application");
             primaryStage.show();
+            
             
         } catch (IOException e) {
             
